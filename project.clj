8ad866d5747078abeed70fbc6df626fb5533ab5c
Chris Howe-Jones <chris.howejones@gmail.com>
2018-12-21 11:30:59 +0000

    Added cloverage to build.

--- project.clj
+++ project.clj
@@ -13,7 +13,8 @@
                  [org.xerial/sqlite-jdbc "3.21.0.1"]
                  [org.clojure/tools.logging "0.4.1"]]
   :plugins [[duct/lein-duct "0.10.6"]
-            [test2junit "1.4.2"]]
+            [test2junit "1.4.2"]
+            [lein-cloverage "1.0.13"]]
   :test2junit-output-dir "test2junit"
   :test2junit-run-ant true
   :main ^:skip-aot film-ratings.main
