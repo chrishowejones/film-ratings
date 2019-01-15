01cdc24f36e0b3887b33ffe29b4a2a874058da67
Chris Howe-Jones <chris.howejones@gmail.com>
2019-01-15 17:07:45 +0000

    Updated dev to remove production sql attributes for postgresql

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -24,18 +24,18 @@
 (when (io/resource "local.clj")
   (load "local"))
 
-(integrant.repl/set-prep! (comp duct/prep read-config))
+(defn remove-prod-database-attributes
+  "The prepared config is a merge of dev and prod config and the prod attributes for everything except :jdbc-url need to be dropped or the sqlite db is configured with postgres attributes"
+  [config]
+  (update config :duct.database.sql/hikaricp (fn [db-config] (->> (find db-config :jdbc-url) (apply hash-map)))))
+
+(integrant.repl/set-prep! (comp remove-prod-database-attributes duct/prep read-config))
 
 (comment
 
   (integrant.repl/go)
   (integrant.repl/reset)
   (integrant.repl/halt)
-
-  (require 'ragtime.core)
-
-  ()
-
 
 
   )
