23f0f3873e1130af0c96dc592254ae40a537b90f
Chris Howe-Jones <chris.howejones@gmail.com>
2019-05-14 20:04:54 +0100

    Updated sleep in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ deploy_cluster() {
                        $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
             echo "Waiting for stale deployments:"
             echo "$stale"
-            sleep 20
+            sleep 30
         else
             echo "Deployed!"
             return 0
